fc5ffa0c506ee2a1c02224ce305af9e94cdb784e
clragon <clragon@clynamic.net>
2026-09-18 10:54:03 +0200

    replaced brand rasters with vectors

--- src/shared/assets.ts
+++ src/shared/assets.ts
@@ -1,8 +1,8 @@
 // Binary brand assets, inlined as data URIs at build time so the bundle is
 // fully self-contained. The `?inline` query is wired up in scripts/build.mjs.
 
-import hexTileUri from "../../assets/hex-tile.png?inline";
-import hexTextureUri from "../../assets/hex-texture.png?inline";
+import hexTileUri from "../../assets/hex-tile.svg?inline";
+import hexTextureUri from "../../assets/hex-texture.svg?inline";
 import logoSvgUri from "../../assets/logo.svg?inline";
 import logoMarkSvgUri from "../../assets/logo-mark.svg?inline";
 import chexagonSvgUri from "../../assets/chexagon.svg?inline";
